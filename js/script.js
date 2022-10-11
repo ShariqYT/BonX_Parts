ce419dbc41b7d1b20af45bc416c4fff785e8fd98
Mohd Shariq <111305981+ShariqYT@users.noreply.github.com>
2022-10-11 07:48:49 +0530

    Update

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu(){
+    subMenu.classList.toggle("open-menu");
+}
+
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value;
+    let emptyArray = [];
+    if(userData){
+        emptyArray = suggestions.filter((data)=>{
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            return data = '<li>'+ data +'</li>';
+        });
+        console.log(emptyArray);
+        searchWrapper.classList.add("active");
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active");
+    }
+}
+
+function select(element){
+    let seletUserData = element.textContent;
+    inputBox.value = seletUserData;
+    searchWrapper.classList.remove("active");
+}
+
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = '<li>'+ userValue +'</li>';
+    }else{
+        listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
---
+++ js/suggestion.js
@@ -0,0 +1,12 @@
+let suggestions = [
+    "Intel core i7-12700",
+    "Intel core i7-12700k",
+    "Intel core i9-12900k",
+    "Intel core i3-10100f",
+    "Intel core i3-12100",
+    "Intel core i5-12500",
+    "AMD Ryzen 5 5600X",
+    "AMD Ryzen 5 3600",
+    "AMD Ryzen 7 5800X",
+    "AMD Ryzen 9 5950X"
+];
